0cd39e5d1d776c499478cd2aa32ef5ed06e332ae
Pride <20098992+PrideInt@users.noreply.github.com>
2024-06-14 13:19:05 -0400

    blast script modifications to attribute for file names

--- blast.sh
+++ blast.sh
@@ -1,3 +1,5 @@
+mkdir -p contigs
+
 # Get all contigs from the contigs folder
 CONTIGS=`ls ./contigs/*.fasta`
 
@@ -12,5 +14,11 @@ for CONTIG in $CONTIGS
 
         # Run blast
         blastn -db database/PLANT/PLANT -query $CONTIG -out ./contigs/$FILE_NAME.txt
+
+        # If you changed the BLAST database name from PLANT/PLANT to something else, you can
+        # comment out the above line and uncomment the below line to change your
+        # script accordingly. Just replace <>/<> with whatevever you named your database.
+
+        # blastn -db database/<>/<> -query $CONTIG -out ./contigs/$FILE_NAME.txt
     done
 
