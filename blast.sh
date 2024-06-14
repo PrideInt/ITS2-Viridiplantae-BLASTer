8415961ac9aeb0cfdb9a71eb0957c5773c697d41
Pride <20098992+PrideInt@users.noreply.github.com>
2024-06-14 13:20:02 -0400

    script parameter fix

--- blast.sh
+++ blast.sh
@@ -1,19 +1,22 @@
+#!/bin/sh
 mkdir -p contigs
 
+# Deprecated:
 # Get all contigs from the contigs folder
-CONTIGS=`ls ./contigs/*.fasta`
+
+# CONTIGS=`ls ./contigs/*.fasta`
 
 # Loop through all contigs
-for CONTIG in $CONTIGS
+for CONTIG in ./contigs/*.fasta
     do
         echo "BLASTING CONTIG:" $CONTIG
 
-        FILE_NAME="$(basename $CONTIG .fasta)"
+        FILE_NAME="$(basename "$CONTIG" .fasta)"
 
         echo "CREATING CONTIG DATA:" $FILE_NAME
 
         # Run blast
-        blastn -db database/PLANT/PLANT -query $CONTIG -out ./contigs/$FILE_NAME.txt
+        blastn -db database/PLANT/PLANT -query "$CONTIG" -out ./contigs/"$FILE_NAME".txt
 
         # If you changed the BLAST database name from PLANT/PLANT to something else, you can
         # comment out the above line and uncomment the below line to change your
